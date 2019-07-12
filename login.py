64c5ea6cfdba4e1b24ea9921e0cb5586b6ed9a4d
ntege <ben.ntegekyu8735@gmail.com>
2019-07-12 12:38:13 +0300

    feat(python_login): this was done using python language

---
+++ login.py
@@ -0,0 +1,9 @@
+# login app in termial using python
+u_name = input("Enter Your Username:")
+
+u_password = input("Enter your password:")
+
+if u_name =="ben":
+	print("Hello" + u_name)
+else:
+	print("Enter proper credentials please")	
